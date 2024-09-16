705988e3cb88a3324ac6727e3e74eb60ee3424e9
katrinajy7 <145580105+katrinajy7@users.noreply.github.com>
2024-09-16 10:12:41 +0800

    corrected

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -68,11 +68,71 @@ import { toast } from 'react-toastify';
 
 
 
+// const ContactForm = () => {
+//   const dispatch = useDispatch();
+//   const contacts = useSelector(getContacts);
+
+//     function handlerSubmit(evt) {
+//     evt.preventDefault();
+//     const form = evt.target;
+//     const name = form.name.value;
+//     const phone = form.number.value;
+
+//     if (
+//       contacts.find(
+//         contacts => contacts.name.toLowerCase() === name.toLowerCase()
+//       )
+//     ) {
+//       return toast.warn(`${name} is alredy in contacts.`);
+//     }
+
+//      dispatch(addContact({ name, phone }));
+//      toast.success(`${name} is added to the contact list!`);
+//      form.reset();
+
+//   }
+   
+//   return (
+//       <form className={css.form} onSubmit={handlerSubmit} autoComplete="off">
+//         <label className={css.label}>
+//           Name
+//           <input
+//             className={css.input}
+//             type="text"
+//             name="name"
+//             placeholder="Ex. Katrina"
+//             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+//           />
+//         </label>
+//         <label className={css.label}>
+//           Number
+//           <input
+//             className={css.input}
+//             type="tel"
+//             name="number"
+//             placeholder="111-11-11"
+//             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+//             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+//             required
+//           />
+//         </label>
+//         <button className={css.btn} type="submit">
+//           Add contact
+//         </button>
+//       </form>
+//     );
+//   }
+
+
+
+// export default ContactForm;
+
 const ContactForm = () => {
   const dispatch = useDispatch();
   const contacts = useSelector(getContacts);
 
-    function handlerSubmit(evt) {
+  function handlerSubmit(evt) {
     evt.preventDefault();
     const form = evt.target;
     const name = form.name.value;
@@ -80,50 +140,47 @@ const ContactForm = () => {
 
     if (
       contacts.find(
-        contacts => contacts.name.toLowerCase() === name.toLowerCase()
+        contact => contact.name.toLowerCase() === name.toLowerCase()
       )
     ) {
-      return toast.warn(`${name} is alredy in contacts.`);
+      return toast.warn(`${name} is already in contacts.`);
     }
 
-     dispatch(addContact({ name, phone }));
-     toast.success(`${name} is added to the contact list!`);
-     form.reset();
-
+    dispatch(addContact({ name, phone }));
+    toast.success(`${name} is added to the contact list!`);
+    form.reset();
   }
-   
+
   return (
-      <form className={css.form} onSubmit={handlerSubmit} autoComplete="off">
-        <label className={css.label}>
-          Name
-          <input
-            className={css.input}
-            type="text"
-            name="name"
-            placeholder="Ex. Katrina"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          />
-        </label>
-        <label className={css.label}>
-          Number
-          <input
-            className={css.input}
-            type="tel"
-            name="number"
-            placeholder="111-11-11"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
-        </label>
-        <button className={css.btn} type="submit">
-          Add contact
-        </button>
-      </form>
-    );
-  }
-
-
+    <form className={css.form} onSubmit={handlerSubmit} autoComplete="off">
+      <label className={css.label}>
+        Name
+        <input
+          className={css.input}
+          type="text"
+          name="name"
+          placeholder="Ex. Katrina"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash, and spaces. For example, Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        />
+      </label>
+      <label className={css.label}>
+        Number
+        <input
+          className={css.input}
+          type="tel"
+          name="number"
+          placeholder="111-11-11"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses, and can start with +"
+          required
+        />
+      </label>
+      <button className={css.btn} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
+}
 
 export default ContactForm;
